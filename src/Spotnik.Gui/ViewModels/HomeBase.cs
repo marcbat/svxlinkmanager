fe7709e4968196a96d6bb59299754122206604c8
Marc Battaglia <marcbat79@gmail.com>
2020-10-11 20:44:24 +0200

    Ajoutez des fichiers projet.

---
+++ src/Spotnik.Gui/ViewModels/HomeBase.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spotnik.Gui.ViewModels
+{
+  public class HomeBase : ComponentBase
+  {
+    private int salon;
+
+    public int Salon { 
+      get => salon; 
+      set { 
+        salon = value;
+
+        RunRestart();
+      } 
+    }
+
+    private void RunRestart()
+    {
+      // Start the child process.
+      Process p = new Process();
+      // Redirect the output stream of the child process.
+      p.StartInfo.UseShellExecute = false;
+      p.StartInfo.RedirectStandardOutput = true;
+      p.StartInfo.FileName = "/etc/spotnik/restart.rrf";
+      p.Start();
+      string output = p.StandardOutput.ReadToEnd();
+      p.WaitForExit();
+    }
+
+    public Dictionary<int, string> Salons => new Dictionary<int, string> { { 1, "Réseau des répéteurs francophones" }, { 2, "Salon suisse romande" } };
+
+  }
+
+}
