4f6cb90c325cc950ed6fa42c8495676656fd4342
Marc Battaglia <marcbat79@gmail.com>
2020-11-03 20:22:51 +0100

    Ok pour la protection partiel par login

---
+++ src/Spotnik.Gui/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Spotnik.Gui.Data;
+
+[assembly: HostingStartup(typeof(Spotnik.Gui.Areas.Identity.IdentityHostingStartup))]
+namespace Spotnik.Gui.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
