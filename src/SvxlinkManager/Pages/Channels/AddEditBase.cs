6a8098d6f9466e6170183d1297bcb3a844a95c09
Marc Battaglia <marcbat79@gmail.com>
2020-11-11 21:30:49 +0100

    User Story 96: Empêcher de temporiser le salon par défaut

--- src/SvxlinkManager/Pages/Channels/AddEditBase.cs
+++ src/SvxlinkManager/Pages/Channels/AddEditBase.cs
@@ -2,17 +2,20 @@
 using SvxlinkManager.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SvxlinkManager.Pages.Channels
 {
-  public abstract class AddEditBase : RepositoryComponentBase
+  public abstract class AddEditBase : RepositoryComponentBase, INotifyPropertyChanged
   {
     [Inject]
     public NavigationManager NavigationManager { get; set; }
 
     protected Channel Channel { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
 
     protected abstract void HandleValidSubmit();
 
