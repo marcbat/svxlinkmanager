04c91d6e540a0dd9042ba41dd4cbd219e035e5ae
Marc Battaglia <marcbat79@gmail.com>
2021-03-27 10:08:34 +0100

    Ajout de la validation pour les premier étapes de l'installeur

---
+++ src/SvxlinkManager/Pages/Installer/ChannelDefaultValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SvxlinkManager.Pages.Installer
+{
+  public class ChannelDefaultValidator : AbstractValidator<InstallerModel>
+  {
+    public ChannelDefaultValidator()
+    {
+      RuleFor(i => i.DefaultChannel).NotEmpty().WithMessage("Vous devez choisir un salon par défaut.");
+    }
+  }
+}
