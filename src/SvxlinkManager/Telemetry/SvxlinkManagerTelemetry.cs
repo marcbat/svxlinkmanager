90b9a01943fd23c01f572500e2542e2efc1d9c09
Marc Battaglia <marcbat79@gmail.com>
2021-09-05 23:12:38 +0200

    Ajout du roleName et role instance dans la telemetry

--- src/SvxlinkManager/Telemetry/SvxlinkManagerTelemetry.cs
+++ src/SvxlinkManager/Telemetry/SvxlinkManagerTelemetry.cs
@@ -12,19 +12,22 @@ namespace SvxlinkManager.Telemetry
   public class SvxlinkManagerTelemetry : TelemetryInitializerBase
   {
     private readonly string deviceId;
+    private readonly string version;
 
     public SvxlinkManagerTelemetry(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
     {
       deviceId = new DeviceIdBuilder().AddMachineName().AddMacAddress().ToString();
+      version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
     }
 
     protected override void OnInitializeTelemetry(HttpContext platformContext, RequestTelemetry requestTelemetry, ITelemetry telemetry)
     {
-      telemetry.Context.GlobalProperties["DeviceId"] = deviceId;
       telemetry.Context.Device.Id = deviceId;
       telemetry.Context.Device.OperatingSystem = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
-      telemetry.Context.Component.Version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+      telemetry.Context.Component.Version = version;
       telemetry.Context.User.AuthenticatedUserId = platformContext.User?.Identity.Name ?? string.Empty;
+      telemetry.Context.Cloud.RoleName = $"SvxlinkManager-{version}";
+      telemetry.Context.Cloud.RoleInstance = $"SvxlinkManager-{deviceId}";
     }
   }
 }
