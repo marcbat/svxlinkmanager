0a4f0face06797892ffbccfe170428d11869394c
Marc Battaglia <marcbat79@gmail.com>
2021-09-26 20:27:43 +0200

    Supression des extensions fr

--- src/SvxlinkManager/Startup.cs
+++ src/SvxlinkManager/Startup.cs
@@ -1,19 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Spotnik.Gui.Areas.Identity;
 using SvxlinkManager.Data;
 using SvxlinkManager.Repositories;
 using SvxlinkManager.Service;
@@ -21,11 +14,7 @@ using System.IO;
 using SvxlinkManager.ServiceMockup;
 using Microsoft.ApplicationInsights.Extensibility;
 using SvxlinkManager.Telemetry;
-using Microsoft.ApplicationInsights;
-using DeviceId;
-using System.Reflection;
 using Microsoft.AspNetCore.Http;
-using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using SvxlinkManager.Areas.Identity;
 
 namespace SvxlinkManager
