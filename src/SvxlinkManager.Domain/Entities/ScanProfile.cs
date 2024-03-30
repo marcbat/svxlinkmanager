c8dcd49cc49ba088649fdeedf8c83a8d340f7cb9
Marc Battaglia <marcbat79@gmail.com>
2024-03-30 13:17:24 +0100

    Ajout de ScanProfil

---
+++ src/SvxlinkManager.Domain/Entities/ScanProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvxlinkManager.Domain.Entities
+{
+    public class ScanProfile : Entity
+    {
+        public ScanProfile(Guid id, string name, int scanDelay): base(id)
+        {
+            Name = name;
+            ScanDelay = scanDelay;
+        }
+
+        public string Name { get; }
+
+        public int ScanDelay { get; }
+
+        public bool Enable { get; set; }
+    }
+}
