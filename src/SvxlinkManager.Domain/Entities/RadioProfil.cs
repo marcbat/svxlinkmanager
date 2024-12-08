33a602ea2540226f36aafb70300e9eee6265d7b5
Marc Battaglia <marcbat79@gmail.com>
2024-12-08 11:33:37 +0100

    Ajout des internal Ctcss

--- src/SvxlinkManager.Domain/Entities/RadioProfil.cs
+++ src/SvxlinkManager.Domain/Entities/RadioProfil.cs
@@ -146,18 +146,18 @@ namespace SvxlinkManager.Domain.Entities
             if (string.IsNullOrWhiteSpace(txCtcss))
                 return Error.New("Le ton de CTCSS de transmission ne peut pas être vide.");
 
-            var valueExist = Ctcss.ContainsValue(txCtcss);
+            var valueExist = Ctcss.ContainsKey(txCtcss);
 
             if(!valueExist)
                 return Error.New("Le ton de CTCSS de transmission n'est pas valide.");
 
-            var keyvalue = Ctcss.Single(x => x.Value == txCtcss);
-
-            return keyvalue.Key;
+            return txCtcss;
         }
 
         public Validation<Error, string> SetTxCtcss(string txCtcss) =>
             ValidateTxCtcss(txCtcss).Map(x => this.txCtcss = x); 
+
+        public string InternalTxCtcss => Ctcss[txCtcss];
 
         public string RxCtCss
         {
@@ -170,60 +170,60 @@ namespace SvxlinkManager.Domain.Entities
             if (string.IsNullOrWhiteSpace(rxCtCss))
                 return Error.New("Le ton de CTCSS de réception ne peut pas être vide.");
 
-            var valueExist = Ctcss.ContainsValue(rxCtCss);
+            var valueExist = Ctcss.ContainsKey(rxCtCss);
 
             if (!valueExist)
                 return Error.New("Le ton de CTCSS de réception n'est pas valide.");
 
-            var keyvalue = Ctcss.Single(x => x.Value == rxCtCss);
-
-            return keyvalue.Key;
+            return rxCtCss;
         }
 
         public Validation<Error, string> SetRxCtCss(string rxCtCss) =>
             ValidateRxCtCss(rxCtCss).Map(x => this.rxCtCss = x);
 
+        public string InternalRxCtCss => Ctcss[rxCtCss];
+
         private static Dictionary<string, string> Ctcss => new()
         {
-          {"0000", "Pas de tone" },
-          {"0001","67"},
-          {"0002","71.9"},
-          {"0003","74.4"},
-          {"0004","77"},
-          {"0005","79.7"},
-          {"0006","82.5"},
-          {"0007","85.4"},
-          {"0008","88.5"},
-          {"0009","91.5"},
-          {"0010","94.8"},
-          {"0011","97.4"},
-          {"0012","100"},
-          {"0013","103.5"},
-          {"0014","107.2"},
-          {"0015","110.9"},
-          {"0016","114.8"},
-          {"0017","118.8"},
-          {"0018","123"},
-          {"0019","127.3"},
-          {"0020","131.8"},
-          {"0021","136.5"},
-          {"0022","141.3"},
-          {"0023","146.2"},
-          {"0024","151.4"},
-          {"0025","156.7"},
-          {"0026","162.2"},
-          {"0027","167.9"},
-          {"0028","173.8"},
-          {"0029","179.9"},
-          {"0030","186.2"},
-          {"0031","192.8"},
-          {"0032","203.5"},
-          {"0033","210.7"},
-          {"0034","218.1"},
-          {"0035","225.7"},
-          {"0036","233.6"},
-          {"0037","241.8"},
-          {"0038","250.3"}
+            { "Pas de tone", "0000" },
+            { "67", "0001" },
+            { "71.9", "0002" },
+            { "74.4", "0003" },
+            { "77", "0004" },
+            { "79.7", "0005" },
+            { "82.5", "0006" },
+            { "85.4", "0007" },
+            { "88.5", "0008" },
+            { "91.5", "0009" },
+            { "94.8", "0010" },
+            { "97.4", "0011" },
+            { "100", "0012" },
+            { "103.5", "0013" },
+            { "107.2", "0014" },
+            { "110.9", "0015" },
+            { "114.8", "0016" },
+            { "118.8", "0017" },
+            { "123", "0018" },
+            { "127.3", "0019" },
+            { "131.8", "0020" },
+            { "136.5", "0021" },
+            { "141.3", "0022" },
+            { "146.2", "0023" },
+            { "151.4", "0024" },
+            { "156.7", "0025" },
+            { "162.2", "0026" },
+            { "167.9", "0027" },
+            { "173.8", "0028" },
+            { "179.9", "0029" },
+            { "186.2", "0030" },
+            { "192.8", "0031" },
+            { "203.5", "0032" },
+            { "210.7", "0033" },
+            { "218.1", "0034" },
+            { "225.7", "0035" },
+            { "233.6", "0036" },
+            { "241.8", "0037" },
+            { "250.3", "0038" }
         };
 
         public string Volume { get; set; }
