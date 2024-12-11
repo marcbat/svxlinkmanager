c190c1305ab70c89e256090200615b5147c070dc
Marc Battaglia <marcbat79@gmail.com>
2024-12-11 21:06:02 +0100

    Ajout de pleins de tests

---
+++ tests/SvxlinkManager.Application.Integration.Tests/SvxlinkManagerConfigs/SvxlinkChannel/Queries/GetAllOriginalChannelsCommandTests.cs
@@ -0,0 +1,47 @@
+﻿using SvxlinkManager.Application.SvxlinkManagerConfigs.Channels.SvxlinkChannel.Queries;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvxlinkManager.Application.Integration.Tests.SvxlinkManagerConfigs.SvxlinkChannel.Queries
+{
+    internal class GetAllOriginalChannelsCommandTests : BaseTest
+    {
+        [Test]
+        public async Task Handle_WhenIsValid_ShouldReturnOriginalChannels()
+        {
+            try
+            {
+                // arrange
+                var install = await CreateDefaultConfigAsync();
+                ClearReceivedCalls();
+
+                // act
+                var command = new GetAllOriginalChannelsCommand();
+                var result = await mediatr.Send(command);
+                
+                // assert
+                var config = svxlinkManagerConfigRepository.GetConfig(configGuid);
+                var calls = GetReceivedCalls();
+                await Verify(new
+                {
+                    result,
+                    config,
+                    calls
+                });
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            finally
+            {
+                File.Delete(db);
+            }
+
+        }
+    }
+}
---
+++ tests/SvxlinkManager.Application.Integration.Tests/SvxlinkManagerConfigs/SvxlinkChannel/Queries/GetSvxlinkChannelByIdQueryTests.cs
@@ -0,0 +1,36 @@
+﻿using SvxlinkManager.Application.SvxlinkManagerConfigs.Channels.SvxlinkChannel.Queries;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvxlinkManager.Application.Integration.Tests.SvxlinkManagerConfigs.SvxlinkChannel.Queries
+{
+    internal class GetSvxlinkChannelByIdQueryTests : BaseTest
+    {
+        [Test]
+        public async Task Handle_WhenIsValid_ShouldReturnSvxlinkChannel()
+        {
+            // Arrange
+            var install = CreateDefaultConfigAsync();
+            ClearReceivedCalls();
+            var query = new GetSvxlinkChannelByIdQuery(configGuid, installChannels.First());
+
+            // Act
+            var result = await mediatr.Send(query);
+            
+            // Assert
+            var config = svxlinkManagerConfigRepository.GetConfig(configGuid);
+            var calls = GetReceivedCalls();
+
+            await Verify(new
+            {
+                result,
+                config,
+                calls
+            });
+        }
+    }
+}
